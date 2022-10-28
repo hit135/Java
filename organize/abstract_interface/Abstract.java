154106d2c1f2e7e1d3bd0f2d63d7c2a258528d2d
Park Hyunggoo <hit135@naver.com>
2022-10-28 21:56:43 +0900

    추상함수 abstracts

---
+++ organize/abstract_interface/Abstract.java
@@ -0,0 +1,22 @@
+package ch10_extends_interface.abstract_interface;
+
+public abstract class Abstract {
+	
+	// 추상 클래스
+	// 추상 메서드
+	
+	// 미완성 클래스와 미완성 메서드가 있다.
+	// 자주 쓰는 함수인데
+	// 결과 값이 다를 때 재정의해서 쓰기 위해 사용
+	
+	// 그런데 이것 보다는 콜백함수에서 사용하는게 보편적
+	
+	public abstract void after();
+	
+	
+	
+	
+	
+	
+
+}
